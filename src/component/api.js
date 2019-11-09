e83ba91a6ae9f19f6b50cea7f8a19dc5cf72b74c
mrcoding <39582062+MingRay98@users.noreply.github.com>
2019-11-10 01:19:46 +0800

    Your awesome message

--- src/component/api.js
+++ src/component/api.js
@@ -2,7 +2,7 @@ import firebase from 'firebase';
 
 let firebaseConfig
 const API = process.env.REACT_APP_WEATHER_API_KEY
-eval('firebaseConfig=' + API);
+// eval('firebaseConfig=' + API);
 // console.log(firebaseConfig)
 
 firebase.initializeApp(firebaseConfig);
